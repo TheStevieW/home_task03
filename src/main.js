eebcbe71e3aad2b2504645d8485fcdca896a7d45
Antons Grigorjevs <work.zolter@mail.ru>
2020-01-28 21:45:52 +0200

    dist update?

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import "./styles/main.pcss";
-if (process.env.NODE_ENV === "development") {
-  require("file-loader!./index.pug");
-}
-
-import "./scripts/parallax.js";
-import "./scripts/skills.js";
-import "./scripts/carousel.js";
